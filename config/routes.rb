4b92c9fa571df519f97bdffb8b2ca3b0295120e1
Charles P <charles.prystupa@gmail.com>
2021-01-05 17:03:12 +0000

    Add Comments Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
-
+  root 'welcome#index'
+  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+#  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -15,7 +18,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :articles
+  
 
   # Example resource route with options:
   #   resources :products do
