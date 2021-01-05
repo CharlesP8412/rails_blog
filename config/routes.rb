12d5b058bff9c5ce1bbd6a3d783d2621faa618a9
Charles P <charles.prystupa@gmail.com>
2021-01-05 16:06:50 +0000

    Form submits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get '/articles/new'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -16,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :articles
 
   # Example resource route with options:
   #   resources :products do
