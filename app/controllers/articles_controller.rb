d97d56e7f82632a3439ce470e638bd0869dfe01a
Charles P <charles.prystupa@gmail.com>
2021-01-05 16:46:11 +0000

    Add: Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,32 +10,40 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-    
-def edit
-  @article = Article.find(params[:id])
-end
- 
- def create
-  @article = Article.new(article_params)
- 
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+
+  def edit
+    @article = Article.find(params[:id])
   end
-end
- 
-def update
-  @article = Article.find(params[:id])
- 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-end
- 
-private
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
   def article_params
     params.require(:article).permit(:title, :text)
   end
